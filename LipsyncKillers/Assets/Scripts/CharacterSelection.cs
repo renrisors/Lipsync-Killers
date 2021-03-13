97795c374711044ed94da36577a92e5f198e1529
Cyber Twilight <gmgabmedeiros@hotmail.com>
2021-03-13 17:48:41 -0300

    Character Select Screen - Done!~

--- LipsyncKillers/Assets/Scripts/CharacterSelection.cs
+++ LipsyncKillers/Assets/Scripts/CharacterSelection.cs
@@ -6,9 +6,15 @@ public class CharacterSelection : MonoBehaviour
     public int selectedCharacter = 0;
     public GameObject[] characters;
      
-    public void SelectCharacter()
+    public void SelectCharacter(string nome)
     {
-        
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (characters[i].name == nome) {
+                selectedCharacter = i;
+                Debug.LogWarning("Personagem passado:" + characters[i].name);
+            }
+        }
     }
 
     public void StartGame()
