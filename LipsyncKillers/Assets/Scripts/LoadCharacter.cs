339d302799c92b3beb644745d6a298dc1df9eb91
Sol <solmseda@gmail.com>
2021-04-16 13:42:20 -0300

    Seleção de Personagem terminada

--- LipsyncKillers/Assets/Scripts/LoadCharacter.cs
+++ LipsyncKillers/Assets/Scripts/LoadCharacter.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class LoadCharacter : MonoBehaviour
 {
     public GameObject[] characterPrefabs;
+    public GameObject[] animationsEnemyPrefabs;
     public Transform spawnPoint;
+    public Transform enemySpawnPoint;
     void Start()
     {
+
+
         int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
-        GameObject prefab = characterPrefabs[selectedCharacter];
-        GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
-        Debug.Log("Personagem carregado: " + characterPrefabs[selectedCharacter].name);
+        GameObject prefabPlayer = characterPrefabs[selectedCharacter];
+        GameObject prefabEnemy;
+        if (selectedCharacter == 0)
+        {
+            prefabEnemy = animationsEnemyPrefabs[1];
+        }
+        else
+        {
+            prefabEnemy = animationsEnemyPrefabs[0];
+        }
+        GameObject player = Instantiate(prefabPlayer, spawnPoint.position, Quaternion.identity);
+        GameObject enemy = Instantiate(prefabEnemy, enemySpawnPoint.position, Quaternion.identity);
 
         BGMusic.Instance.gameObject.GetComponent<AudioSource>().Stop();
     }
