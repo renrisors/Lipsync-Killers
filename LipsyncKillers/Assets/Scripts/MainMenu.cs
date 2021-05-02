719a6ded84eb1d5453227a37fa6e62aaf178c80b
Cyber Twilight <gmgabmedeiros@hotmail.com>
2021-05-01 21:24:44 -0300

    TutorialPanel skeleton on CharSelect screen + ButtonsSFX Fix [Finally!!]

--- LipsyncKillers/Assets/Scripts/MainMenu.cs
+++ LipsyncKillers/Assets/Scripts/MainMenu.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private string scene = "scene";
     
     public void ChangeScene(string SceneName)
     {
-        SceneManager.LoadScene(SceneName);
+        scene = SceneName;
+        StartCoroutine(SceneDelay());
     }
 
 
     public void ExitGame()
     {
+        StartCoroutine(SceneDelay());
         Application.Quit();
     }
+
+    public IEnumerator SceneDelay()
+    {
+        yield return new WaitForSeconds(0.07f);
+        SceneManager.LoadScene(scene);
+    }
 }
+    
